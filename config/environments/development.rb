14e55c1310d2f49f0830e287d55ed1382925cec4
RameshK <rameshkror@gmail.com>
2020-06-08 06:39:45 +0530

    Production 9

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,12 +53,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:        'smtp.sendgrid.net',
+    address:        'smtp.gmail.com',
     port:           587,
     username:       ENV['SMTP_USERNAME'],
     password:       ENV['SMTP_PASSWORD'],
     domain:         ENV['SMTP_DOMAIN'],
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true
   }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,12 +97,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:        ENV['SMTP_ADDR'],
+    address:        'smtp.gmail.com',
     port:           587,
     username:       ENV['SMTP_USERNAME'],
     password:       ENV['SMTP_PASSWORD'],
     domain:         ENV['SMTP_DOMAIN'],
-    authentication: :plain
+    authentication: :login
   }
   config.action_mailer.default_url_options = {
     :host => 'gtb4za.herokuapp.com'
