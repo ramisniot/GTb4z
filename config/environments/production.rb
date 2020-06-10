ba1398436194ab390b01a83c9ab9013269b7eec8
RameshK <rameshkror@gmail.com>
2020-06-10 08:37:14 +0530

    Production 13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'gtb4za.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
