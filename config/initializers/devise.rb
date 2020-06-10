33fa0d06938f738cb68ddd51540526c0338cd720
RameshK <rameshkror@gmail.com>
2020-06-10 14:26:08 +0530

    Production 23

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = ENV['DEVISE_EMAIL'] || 'no-reply@heroku.com'
+  config.mailer_sender = ENV['DEVISE_EMAIL'] || 'no-reply@sparkpostmail.com'
   # config.mailer_sender = ENV['DEVISE_EMAIL'] || 'rameshkror@gmail.com'
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
