2623a3c4598fcef8435f02f640dec8fe424175ce
RameshK <rameshkror@gmail.com>
2020-06-12 08:00:43 +0530

    Production 25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,12 +97,18 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:        ENV['SMTP_HOST'],
-    port:           587,
-    username:       ENV['SMTP_USERNAME'],
-    password:       ENV['SMTP_PASSWORD'],
-    domain:         ENV['SMTP_DOMAIN'],
-    authentication: :login,
+    # address:        ENV['SMTP_HOST'],
+    # port:           587,
+    # username:       ENV['SMTP_USERNAME'],
+    # password:       ENV['SMTP_PASSWORD'],
+    # domain:         ENV['SMTP_DOMAIN'],
+    # authentication: :login,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'gtbforza.herokuapp.com',
+    :authentication => :plain,
     enable_starttls_auto: true
   }
 
--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,12 +1,22 @@
 email_config =  YAML.load(File.read('config/email.yml'))[Rails.env] if File.exist?('config/email.yml')
 email_config ||= {}
 
+# ActionMailer::Base.smtp_settings = {
+#   port:           ENV['SMTP_PORT']     || email_config['SMTP_PORT']      || 587,
+#   address:        ENV['SMTP_HOST']     || email_config['SMTP_HOST']      || 'smtp.sparkpostmail.com',
+#   user_name:      ENV['SMTP_USERNAME'] || email_config['SMTP_USERNAME']  || 'SMTP_Injection',
+#   password:       ENV['SMTP_PASSWORD'] || email_config['SMTP_PASSWORD'],
+#   domain:         ENV['SMTP_DOMAIN']   || email_config['SMTP_DOMAIN']    || 'byteforza.com',
+#   tls:            ENV['SMTP_TLS']      || email_config['SMTP_TLS']       || false,
+#   authentication: :plain
+# }
+
 ActionMailer::Base.smtp_settings = {
-  port:           ENV['SMTP_PORT']     || email_config['SMTP_PORT']      || 587,
-  address:        ENV['SMTP_HOST']     || email_config['SMTP_HOST']      || 'smtp.sparkpostmail.com',
-  user_name:      ENV['SMTP_USERNAME'] || email_config['SMTP_USERNAME']  || 'SMTP_Injection',
-  password:       ENV['SMTP_PASSWORD'] || email_config['SMTP_PASSWORD'],
-  domain:         ENV['SMTP_DOMAIN']   || email_config['SMTP_DOMAIN']    || 'byteforza.com',
-  tls:            ENV['SMTP_TLS']      || email_config['SMTP_TLS']       || false,
-  authentication: :plain
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'gtbforza.herokuapp.com',
+  :authentication => :plain
 }
+# ActionMailer::Base.delivery_method = :smtp
